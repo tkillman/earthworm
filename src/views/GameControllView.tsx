bd2a7eba30a73c22eaa4047b7e79bffb4d05bb2c
김 동규 <tkillman@logiall.com>
2023-09-13 17:41:52 +0900

    키보드 위치 수정

--- src/views/GameControllView.tsx
+++ src/views/GameControllView.tsx
@@ -72,7 +72,7 @@ const GameControllView: React.FC<IProps> = (props) => {
             onClick={onClickKeyBoard(MOVE_DIRECTION.UP)}
           ></img>
         </div>
-        <div className="flex gap-24">
+        <div className="flex gap-20">
           <img
             src={arrowLeft}
             alt="left"
